6f6a0befaa9af3327885e3d29494b8b161b6ddd2
Junha Park <bd08001@gmail.com>
2025-04-23 02:19:33 +0800

    Add keymap to toggle diagnostic

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,14 @@ vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k")
 vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j")
 vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h")
 vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l")
+local wk = require("which-key")
+wk.add({
+  {
+    "<leader>xo",
+    function()
+      vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+    end,
+    desc = "toggle diagnostics",
+    mode = "n",
+  },
+})
