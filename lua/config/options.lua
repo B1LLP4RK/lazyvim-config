37a0afdbf17a9d688f4033e039bf212778b7ce3c
Junha Park <bd08001@gmail.com>
2025-04-29 22:50:14 +0800

    setup lua language server
    mason and lspconfig plugin doesn't seem to work, so downloaded
    lua-language-server from the pkg command

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,28 @@ vim.g.tex_conceal = "abdmg"
 vim.opt.filetype = "plugin"
 vim.opt.filetype = "indent"
 vim.g.mkdp_images_path = "/mnt/c/Users/bd080/OneDrive - National University of Singapore/Desktop/obs"
+vim.lsp.config['luals'] = {
+  -- Command and arguments to start the server.
+  cmd = { 'lua-language-server' },
+
+  -- Filetypes to automatically attach to.
+  filetypes = { 'lua' },
+
+  -- Sets the "root directory" to the parent directory of the file in the
+  -- current buffer that contains either a ".luarc.json" or a
+  -- ".luarc.jsonc" file. Files that share a root directory will reuse
+  -- the connection to the same LSP server.
+  root_markers = { '.luarc.json', '.luarc.jsonc' },
+
+  -- Specific settings to send to the server. The schema for this is
+  -- defined by the server. For example the schema for lua-language-server
+  -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  }
+}
+vim.lsp.enable('luals')
