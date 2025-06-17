4453b3ca8765f5399d34a9a3853a8f04689e7007
Junha Park <bd08001@gmail.com>
2025-06-17 19:56:23 +0800

    change windows treesitter to use curl and tar instead of git as recommended
    https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = false
     table.insert(opts.ensure_installed, "latex")
   end,
 }
