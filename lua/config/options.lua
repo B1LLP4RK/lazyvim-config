57352f599ad08b89994be537aa88512eca84a4e6
Junha Park <bd08001@gmail.com>
2026-08-13 12:27:29 +0800

    remove obsolete config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,18 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.clipboard = {
-  name = "Win32Yank",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = 0,
-}
 vim.opt.clipboard:append({ "unnamedplus" })
 
 vim.g.tex_flavor = "latex"
@@ -22,7 +10,6 @@ vim.opt.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.opt.filetype = "plugin"
 vim.opt.filetype = "indent"
-vim.g.mkdp_images_path = "/mnt/c/Users/bd080/OneDrive - National University of Singapore/Desktop/obs"
 vim.keymap.set("n", "<leader>ce", ":%s/\\r//g<cr>", { desc = "remove carriage retunr line feed" })
 -- only for windows
 vim.o.shell = "powershell"
